65b35693f6cb5b92d0eb1027e49f4a556418a510
dfireBird <me@dfirebird.dev>
2023-07-20 21:04:23 +0530

    refactor: reduce code duplication for finding values in decode

--- src/decode.rs
+++ src/decode.rs
@@ -7,20 +7,19 @@ pub fn decode_op(first_byte: u8, second_byte: u8) -> OpCode {
     let first_nibble = |byte| (byte & 0xF0) >> 4;
     let second_nibble = |byte| byte & 0x0F;
 
-    // nnn is only used in few opcodes. Moving it here reduces code duplication
+    let x = second_nibble(first_byte);
+    let y = first_nibble(second_byte);
+    let n = second_nibble(second_byte);
+    let nn = second_byte;
     let nnn = u16::from_be_bytes([second_nibble(first_byte), second_byte]);
 
     match first_nibble(first_byte) {
         0x00 if second_byte == 0xE0 => OpCode::Cls,
         0x01 => OpCode::Jmp(nnn),
-        0x06 => OpCode::Set(second_nibble(first_byte), second_byte),
-        0x07 => OpCode::AddImd(second_nibble(first_byte), second_byte),
+        0x06 => OpCode::Set(x, nn),
+        0x07 => OpCode::AddImd(x, nn),
         0x0A => OpCode::SetIndex(nnn),
-        0x0D => OpCode::Disp(
-            second_nibble(first_byte),
-            first_nibble(second_byte),
-            second_nibble(second_byte),
-        ),
+        0x0D => OpCode::Disp(x, y, n),
         _ => OpCode::NotImplemented,
     }
 }
